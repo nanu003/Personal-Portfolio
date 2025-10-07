e9c8de7f2fdfa866c9797fc9d768be78eaf0ac55
Nikhil Kalkhuria <151564508+nanu003@users.noreply.github.com>
2025-10-07 12:30:41 +0530

    Simplify Vite configuration by removing base and build options
    Removed base path and build configuration for assets.

--- Personal-Portfolio/vite.config.js
+++ Personal-Portfolio/vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: './',  
   plugins: [react(), tailwindcss()],
-   build: {
-    assetsDir: 'assets', // Ensure this matches the asset directory name
-    // This is the property that often needs to be set for GitHub Pages compatibility
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
 })
